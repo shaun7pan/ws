d9d9fd9ff9c0c088ec6f340867a63715c4c7a26b
Pan Shaun <Shaun.Pan@fil.com>
2023-05-14 13:06:23 +0800

    Finish 9 - add teacher features

--- webservice/src/routers.rs
+++ webservice/src/routers.rs
@@ -1,6 +1,11 @@
 use crate::handlers::course::*;
 use crate::handlers::general::*;
+use crate::handlers::teacher::delete_teacher;
+use crate::handlers::teacher::get_all_teachers;
+use crate::handlers::teacher::get_teacher_details;
+use crate::handlers::teacher::post_new_teacher;
 use actix_web::web;
+use actix_web::web::post;
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
@@ -20,5 +25,16 @@ pub fn course_routes(cfg: &mut web::ServiceConfig) {
                 "/{teacher_id}/{course_id}",
                 web::put().to(update_course_details),
             ),
+    );
+}
+
+pub fn teacher_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/teachers")
+            .route("/", web::post().to(post_new_teacher))
+            .route("/", web::get().to(get_all_teachers))
+            .route("/{teacher_id}", web::get().to(get_teacher_details))
+            .route("/{teacher_id}", web::put().to(update_course_details))
+            .route("/{teacher_id}", web::delete().to(delete_teacher)),
     );
 }
